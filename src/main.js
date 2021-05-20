60ff3b2bea6d1e80451e30d21847e3b5f859aa9d
bobycat22 <anthonyznj@gmail.com>
2021-05-20 09:28:11 -0400

    change axios baseurl

--- src/main.js
+++ src/main.js
@@ -22,7 +22,8 @@ const options = {
   inverse: false
 }
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+//axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://onefocus.heroku.com'
 
 // icons, css
 import '@fortawesome/fontawesome-free/css/all.css'
